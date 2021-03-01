01668e1cde10e2a597e7cc0e617f387a615ff3be
Daniel Vaknin <danielvaknin10@gmail.com>
2021-03-01 23:10:56 +0200

    Fix recycler view

---
+++ app/src/main/java/com/daniel/mamafood/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,70 @@
+package com.daniel.mamafood.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.daniel.mamafood.R;
+import com.daniel.mamafood.model.Meal;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    Context context;
+    List<Meal> mealArrayList;
+
+    public RecyclerViewAdapter(Context context, List<Meal> mealArrayList) {
+        this.context = context;
+        this.mealArrayList = mealArrayList;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(context).inflate(R.layout.meal_list_row, parent, false);
+        return new RecyclerViewViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        Meal meal = mealArrayList.get(position);
+        RecyclerViewViewHolder viewHolder = (RecyclerViewViewHolder) holder;
+
+        viewHolder.txtView_name.setText(meal.getName());
+        viewHolder.txtView_price.setText(meal.getPrice().toString());
+
+        // Get image from Firestore using Picasso
+        viewHolder.imgView_icon.setImageResource(R.drawable.meal_placeholder); // So that we'll first show the empty avatar as the load takes time
+        if (meal.getImageUrl() != null) {
+            Picasso.get().load(meal.getImageUrl()).placeholder(R.drawable.meal_placeholder).into(viewHolder.imgView_icon);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mealArrayList.size();
+    }
+
+    class RecyclerViewViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgView_icon;
+        TextView txtView_name;
+        TextView txtView_price;
+
+        public RecyclerViewViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgView_icon = itemView.findViewById(R.id.meal_row_image);
+            txtView_name = itemView.findViewById(R.id.meal_row_name);
+            txtView_price = itemView.findViewById(R.id.meal_row_price);
+        }
+    }
+}
