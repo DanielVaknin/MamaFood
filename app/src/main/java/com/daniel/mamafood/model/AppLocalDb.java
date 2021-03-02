719bd52b7e3348dee8ab9832d6ae7cb91081d0ae
Daniel Vaknin <danielvaknin10@gmail.com>
2021-03-03 00:37:47 +0200

    Raise DB level to clean it

--- app/src/main/java/com/daniel/mamafood/model/AppLocalDb.java
+++ app/src/main/java/com/daniel/mamafood/model/AppLocalDb.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 
 import com.daniel.mamafood.MyApplication;
 
-@Database(entities = {Meal.class}, version = 5)
+@Database(entities = {Meal.class}, version = 6)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract MealDao mealDao();
 }
